ba01481a1ea041b3084e5d50ee3cf637ede7973f
zzy1987 <111731369+zzy1987@users.noreply.github.com>
2022-10-24 22:37:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4epy6GlyLjFCeuKYuRUBDsccFRI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TBEL8a-Ko4d4IJs7mEJw77zlaz4Jcc82F2HbGkVtMJc',
+      useTemplateId: '39VCMTdGOvl7GMdSZcfgLTn9PGkhw_xs9nAMNIhz8oM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-01',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '39VCMTdGOvl7GMdSZcfgLTn9PGkhw_xs9nAMNIhz8oM',
 
   CALLBACK_USERS: [
     {
